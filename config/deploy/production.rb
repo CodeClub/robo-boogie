6de7ba980a7fa4e512819cd059f9aaa34b101941
Greg Annandale <g.annandale@gmail.com>
2015-10-12 07:25:19 -0400

    Updated deploy path

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@ server 'roboboogie.codeclub.org.uk', user: 'deploy', roles: [:web, :app, :db], p
 
 set :application,        'roboboogie.codeclub.org.uk'
 set :branch,             'master'
-set :deploy_to,          '/var/www/roboboogie.codeclub.org.uk/static'
+set :deploy_to,          '/var/www/roboboogie.codeclub.org.uk'
