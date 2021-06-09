3b507b0d962cf6134986fb7b90ed1095bd9d3a71
Greg Annandale <greg@raspberrypi.org>
2021-06-09 14:20:24 +0100

    Fix quotes

--- api/index.php
+++ api/index.php
@@ -2,7 +2,7 @@
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: origin, x-requested-with, content-type');
-header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS");
+header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS');
 
 /*
  *---------------------------------------------------------------
