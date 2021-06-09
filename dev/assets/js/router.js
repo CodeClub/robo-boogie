ae55502a31337c8bb12e06233dabe074b7e68392
Greg Annandale <greg@raspberrypi.org>
2021-06-09 14:35:44 +0100

    Debug

--- dev/assets/js/router.js
+++ dev/assets/js/router.js
@@ -132,26 +132,9 @@ define(['backbone',
 
                 self.user.set({'uuid': userID});
 
-                self.user.save(null, {
+                localStorage.setItem('codeclub_userID', userID);
 
-                    success: function (model, response) {
-
-                        // Set the ID
-                        localStorage.setItem('codeclub_userID', userID);
-
-                        self.startHistory();
-
-                    },
-                    error: function (error, resp) {
-
-                        console.error(error);
-                        console.log(resp);
-
-                        alert("Unable to save you as a user");
-
-                    }
-
-                });
+                self.startHistory();
 
             } else {
                 
