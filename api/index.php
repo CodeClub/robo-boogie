3e5b0be7e525f2562aeae4a1d58bf690e7348e6c
Greg Annandale <greg@raspberrypi.org>
2021-06-09 14:05:15 +0100

    Set CORS headers

--- api/index.php
+++ api/index.php
@@ -1,5 +1,9 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: origin, x-requested-with, content-type');
+header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS");
+
 /*
  *---------------------------------------------------------------
  * APPLICATION ENVIRONMENT
